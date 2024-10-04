59970823e2fe174e034b7ae7ee0d351420c07f3d
Michael Welch <michaelgwelch@gmail.com>
2024-10-04 17:15:47 -0500

    upgrade to dotnet 8

--- code/AdvancedGaugeControlVB/TestClient/AssemblyInfo.vb
+++ code/AdvancedGaugeControlVB/TestClient/AssemblyInfo.vb
@@ -1,31 +1,7 @@
 Imports System.Reflection
 Imports System.Runtime.InteropServices
-
-' General Information about an assembly is controlled through the following 
-' set of attributes. Change these attribute values to modify the information
-' associated with an assembly.
-
-' Review the values of the assembly attributes
-
-<Assembly: AssemblyTitle("")> 
-<Assembly: AssemblyDescription("")> 
-<Assembly: AssemblyCompany("")> 
-<Assembly: AssemblyProduct("")> 
-<Assembly: AssemblyCopyright("")> 
 <Assembly: AssemblyTrademark("")> 
 <Assembly: CLSCompliant(True)> 
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
 <Assembly: Guid("80808CA3-B8F5-421A-8A70-7C5A7F887786")> 
-
-' Version information for an assembly consists of the following four values:
-'
-'      Major Version
-'      Minor Version 
-'      Build Number
-'      Revision
-'
-' You can specify all the values or you can default the Build and Revision Numbers 
-' by using the '*' as shown below:
-
-<Assembly: AssemblyVersion("1.0.*")> 
